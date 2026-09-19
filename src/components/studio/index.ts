f25a3cad07aab4dee82d82d46c88a6fa3e7dbb45
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 13:16:19 +0100

    s

--- src/components/studio/index.ts
+++ src/components/studio/index.ts
@@ -1,3 +1,5 @@
 export { ArViewer } from "./ArViewer";
 export { CatalogPanel } from "./CatalogPanel";
+export { EcosystemBar } from "./EcosystemBar";
+export { EcosystemBar } from "./EcosystemBar";
 export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
