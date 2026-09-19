cb6ececb10fbe5daeaae7cbeba83977293b6d635
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 13:39:53 +0100

    s

--- src/components/studio/index.ts
+++ src/components/studio/index.ts
@@ -4,4 +4,6 @@ export { EcosystemBar } from "./EcosystemBar";
 export { IconButton } from "./IconButton";
 export { InspectorPanel } from "./InspectorPanel";
 export { NumericControl } from "./NumericControl";
+export { ProjectFileImport } from "./ProjectFileImport";
+export { ShareDialog } from "./ShareDialog";
 export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
