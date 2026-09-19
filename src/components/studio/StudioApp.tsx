6f3b4db9355fc4a1736c56c139c94e46885abd16
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 14:03:15 +0100

    s

--- src/components/studio/StudioApp.tsx
+++ src/components/studio/StudioApp.tsx
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 import { ImagePlus } from "lucide-react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { CatalogPanel, EcosystemBar, InspectorPanel, ShareDialog, SidePanel, Toolbar } from "./";
-import { Scene3D, type SceneApi } from "./scene/Scene3D";
+import { Scene3D, type SceneApi } from "./scene";
 import { useStudio } from "@/lib/studio/store";
 import { getModel, resolvePreset } from "@/lib/studio/models";
 import { ACCEPTED_IMAGE, downloadDataUrl, exportProjectFile, isImageFile, readArtwork, readProjectFile, saveShared, slugify } from "@/lib/studio/project";
