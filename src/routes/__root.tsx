913ed9c598bb3dbebbc3e86959f06ff0c17920a0
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 11:38:03 +0100

    s

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {
-  Outlet,
-  Link,
-  createRootRouteWithContext,
-  useRouter,
-  HeadContent,
-  Scripts,
-} from "@tanstack/react-router";
-import { useEffect, type ReactNode } from "react";
-
+import { Outlet, createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/react-router";
+import { type ReactNode } from "react";
+import { Toaster } from "sonner";
 import appCss from "../styles.css?url";
 import { Toaster } from "@/components/ui/sonner";
 import { useStudio } from "@/lib/studio/store";
