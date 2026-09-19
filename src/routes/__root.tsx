d3db33b8f20239a18b1af5edcfe2005aab530e09
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 13:00:32 +0100

    S

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -83,7 +83,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 
 function RootShell({ children }: { children: ReactNode }) {
   return (
-    <html lang="fr" className="dark">
+    <html lang="fr" className="light">
       <head>
         <HeadContent />
       </head>
