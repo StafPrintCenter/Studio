5f866bc53fa13ea623a38543d26e706b5f5aad8b
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 12:29:23 +0100

    s

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,6 @@ import { SITE } from "@/data/site";
 const PAGE_TITLE = `${SITE.tool} - Outils de modélisation 3D et de réalité augmentée | ${SITE.name}`;
 const PAGE_DESC = `Studio 3D professionnel pour prévisualiser vos supports imprimés, générer un BAT et les voir en réalité augmentée ${SITE.name}.`;
 
-
 export const Route = createFileRoute("/")({
   ssr: false,
   head: () => ({
