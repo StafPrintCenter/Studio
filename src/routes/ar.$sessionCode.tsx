d525a18259aa330596c84c90f0257a2308eaa0b1
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 12:30:57 +0100

    s

--- src/routes/ar.$sessionCode.tsx
+++ src/routes/ar.$sessionCode.tsx
@@ -6,6 +6,10 @@ import { ProjectFileImport } from "@/components/studio/ProjectFileImport";
 import { getModel, resolvePreset } from "@/lib/studio/models";
 import { loadShared, type SharedRecord } from "@/lib/studio/project";
 import type { ProjectState } from "@/lib/studio/store";
+import { SITE } from "@/data/site";
+
+const PAGE_TITLE = `${SITE.tool} - Outils de modélisation 3D et de réalité augmentée | ${SITE.name}`;
+const PAGE_DESC = `Studio 3D professionnel pour prévisualiser vos supports imprimés, générer un BAT et les voir en réalité augmentée ${SITE.name}.`;
 
 export const Route = createFileRoute("/ar/$sessionCode")({
   ssr: false,
