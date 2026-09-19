adcefdc77307199eb3cdddc0164397b329de84ce
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 12:27:21 +0100

    d

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { StudioApp } from "@/components/studio/StudioApp";
+import { SITE, SITE_LINK } from "@/data/site";
+
+const PAGE_TITLE = `${SITE.tool} - Outils de modélisation 3D et de réalité augmentée | ${SITE.name}`;
+const PAGE_DESC = `Studio 3D professionnel pour prévisualiser vos supports imprimés, générer un BAT et les voir en réalité augmentée ${SITE.name}.`;
+
 
 export const Route = createFileRoute("/")({
   ssr: false,
   head: () => ({
     meta: [
-      { title: "SPC 3D Studio & AR - Simulateur 3D STAF PRINT CENTER" },
+      { title: PAGE_TITLE },
       {
         name: "description", content:
           PAGE_DESC
