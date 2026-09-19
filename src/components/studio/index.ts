58015e694c2389c75ef7144ce3f9977becb49844
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 13:34:11 +0100

    NumericControl

--- src/components/studio/index.ts
+++ src/components/studio/index.ts
@@ -2,4 +2,6 @@ export { ArViewer } from "./ArViewer";
 export { CatalogPanel } from "./CatalogPanel";
 export { EcosystemBar } from "./EcosystemBar";
 export { IconButton } from "./IconButton";
+export { InspectorPanel } from "./InspectorPanel";
+export { NumericControl } from "./NumericControl";
 export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
