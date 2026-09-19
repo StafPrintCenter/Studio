8ba671ecfff4fb294681e7090ee370ccc789ded0
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 13:13:53 +0100

    s

--- src/routes/ar.$sessionCode.tsx
+++ src/routes/ar.$sessionCode.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { Box, Ruler, ArrowLeft } from "lucide-react";
-import { ArViewer } from "@/components/studio/ArViewer";
+import { ArViewer } from "@/components/studio";
 import { ProjectFileImport } from "@/components/studio/ProjectFileImport";
 import { getModel, resolvePreset } from "@/lib/studio/models";
 import { loadShared, type SharedRecord } from "@/lib/studio/project";
