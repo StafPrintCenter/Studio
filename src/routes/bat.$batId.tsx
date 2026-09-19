91b33c493814b0bf566067dbbf8abc7936364a54
steveasterafovo <steveasterafovo@gmail.com>
2026-09-19 12:35:11 +0100

    d

--- src/routes/bat.$batId.tsx
+++ src/routes/bat.$batId.tsx
@@ -1,16 +1,6 @@
 import { useEffect, useState } from "react";
 import { createFileRoute, Link } from "@tanstack/react-router";
-import {
-  BadgeCheck,
-  Box,
-  CheckCircle2,
-  Download,
-  Mail,
-  Ruler,
-  Share2,
-  Sparkles,
-  Smartphone,
-} from "lucide-react";
+import { BadgeCheck, Box, CheckCircle2, Download, Mail, Ruler, Share2, Sparkles, Smartphone } from "lucide-react";
 import { toast } from "sonner";
 import { Scene3D } from "@/components/studio/scene/Scene3D";
 import { ProjectFileImport } from "@/components/studio/ProjectFileImport";
